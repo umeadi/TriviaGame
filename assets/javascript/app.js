8107c36b8fc11b30efa9d0cb5a3154e0df4c590c
Umeadi, Marlon <umeadi@gmail.com>
2019-05-31 01:53:18 -0500

    fixed syntax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,34 +16,57 @@
 
 
 
-
+// On click function to start game
 $("#start").on("click",function(){
-    for(var i=0;i<questions.length;i++){
-        $("#subwrapper").append("<h2>"+questions[i].question+"<h2>");
-        for(var q=0;q<questions[i].answers.length;q++){
-            $("#subwrapper").append("<input type='radio' name='question-" +i+"'value='"+questions[i].answers[q]+"'>"+questions[i].answers[q])
-        }
-    };
+    game.start();
 })
 
+
+// Question Array containing questions and answers
 var questions = [{
-    question:"Where did Tony Stark build his first Iron Man suit?",
-    answers:["On an island ", "In a cave...with a box of scraps ", "In his garage ", "At his private research facility"],
-    correctAnswer:"In a cave...with a box of scraps"
+    question: "Where did Tony Stark build his first Iron Man suit?",
+    answers: ["On an island ", "In a cave...with a box of scraps ", "In his garage ", "At his private research facility"],
+    correctAnswer: "In a cave...with a box of scraps"
 }, {
-    question:"What color was Wolverine's original costume?",
-    answers:["Black and grey ", "Yellow and blue ", "Brown and yellow ", "Red and white"],
-    correctAnswer:"Yellow and blue"
+    question: "What color was Wolverine's original costume?",
+    answers: ["Black and grey ", "Yellow and blue ", "Brown and yellow ", "Red and white"],
+    correctAnswer: "Yellow and blue"
 }, {
-    question:"What material is Captain America's shield made of?",
-    answers:["Titanium ", "Unobtainum ", "Vibranium ", "Plutonium"],
-    correctAnswer:"Vibranium"
+    question: "What material is Captain America's shield made of?",
+    answers: ["Titanium ", "Unobtainum ", "Vibranium ", "Plutonium"],
+    correctAnswer: "Vibranium"
 }, {
-    question:"What land is The Black Panther King of?",
-    answers:["Wakanda ", "Botswana ", "Onitsha ", "Wukari"],
-    correctAnswer:"Wakanda"
+    question: "What land is The Black Panther King of?",
+    answers: ["Wakanda ", "Botswana ", "Onitsha ", "Wukari"],
+    correctAnswer: "Wakanda"
 }, {
-    question:"What is the Silver Surfer's real name?",
-    answers:["Zolan Ladd ", "Bill Bixby ","Hal Jordan ", "Norrin Radd"],
-    correctAnswer:"Norrin Radd"
+    question: "What is the Silver Surfer's real name?",
+    answers: ["Zolan Ladd ", "Bill Bixby ", "Hal Jordan ", "Norrin Radd"],
+    correctAnswer: "Norrin Radd"
 }];
+
+// Object containing game key/value pairs
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function () {
+        game.counter--;
+        $("#counter").html(game.counter);
+        if (game.counter <= 0) {
+            console.log("Time's Up!");
+            game.done();
+        }
+    },
+
+// Start game function to update html on page with questions 
+    start: function () {
+        $("#start").remove();
+        for (var i = 0; i < questions.length; i++) {
+            $("#subwrapper").append("<h2>" + questions[i].question + "<h2>");
+            for (var q = 0; q < questions[i].answers.length; q++) {
+                $("#subwrapper").append("<input type='radio' name='question-" + i + "'value='" + questions[i].answers[q] + "'>" + questions[i].answers[q])
+            }
+        }
+    }
+}
