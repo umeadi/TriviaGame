58be9241a2a19e5fca9dd0d7ba129ce058ad0102
Umeadi, Marlon <umeadi@gmail.com>
2019-05-31 01:18:55 -0500

    fixed radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,27 +18,32 @@
 
 
 $("#start").on("click",function(){
-    $("#subwrapper").remove();
+    for(var i=0;i<questions.length;i++){
+        $("#subwrapper").append("<h2>"+questions[i].question+"<h2>");
+        for(var q=0;q<questions[i].answers.length;q++){
+            $("#subwrapper").append("<input type='radio' name='question-" +i+"'value='"+questions[i].answers[q]+"'>"+questions[i].answers[q])
+        }
+    };
 })
 
 var questions = [{
     question:"Where did Tony Stark build his first Iron Man suit?",
-    answers:["On an island", "In a cave...with a box of scraps", "In his garage", "At his private research facility"],
+    answers:["On an island ", "In a cave...with a box of scraps ", "In his garage ", "At his private research facility"],
     correctAnswer:"In a cave...with a box of scraps"
 }, {
     question:"What color was Wolverine's original costume?",
-    answers:["Black and grey", "Yellow and blue", "Brown and yellow", "Red and white"],
+    answers:["Black and grey ", "Yellow and blue ", "Brown and yellow ", "Red and white"],
     correctAnswer:"Yellow and blue"
 }, {
     question:"What material is Captain America's shield made of?",
-    answers:["Titanium", "Unobtainum", "Vibranium", "Plutonium"],
+    answers:["Titanium ", "Unobtainum ", "Vibranium ", "Plutonium"],
     correctAnswer:"Vibranium"
 }, {
-    question:"What land is The Black Panther King of",
-    answers:["Wakanda", "Botswana", "Onitsha", "Wukari"],
-    correctAnswer:"In a cave...with a box of scraps"
+    question:"What land is The Black Panther King of?",
+    answers:["Wakanda ", "Botswana ", "Onitsha ", "Wukari"],
+    correctAnswer:"Wakanda"
 }, {
-    question:"Where did Tony Stark build his first Iron Man suit?",
-    answers:["On an island", "In his garage", "In a cave...with a box of scraps"],
-    correctAnswer:"In a cave...with a box of scraps"
+    question:"What is the Silver Surfer's real name?",
+    answers:["Zolan Ladd ", "Bill Bixby ","Hal Jordan ", "Norrin Radd"],
+    correctAnswer:"Norrin Radd"
 }];
